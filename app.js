a44e5e6442d2c0ddfc96acdf0b5f789fb6757012
nadasamyy <119449041+nadasamyy@users.noreply.github.com>
2023-08-15 21:02:55 +0300

    task6

--- app.js
+++ app.js
@@ -1,54 +1,126 @@
-const fs = require("fs") ; 
-const alldata = require("./alldata")
-const addperson = (fname , lname , age , city , id) =>{
-    const alldata = loadData() //obj
-    alldata.push ({
-        fname :fname , 
-        lname :lname , 
-        age:age , 
-        city:city,
-        id : id 
-    })
-    savedata(alldata)
-}
-const loadData = ()=>{ //load el data in json
-    try{
-    const jsonfile =  fs.readFileSync("data.json").toString();
-    return JSON.parse(jsonfile);
+const mongodb = require("mongodb")
+const MongoClient = mongodb.MongoClient 
+const connectionurl = "mongodb://127.0.0.1:27017"
+const dbname = "project1"
+MongoClient.connect(connectionurl , (error , data) =>{
+    if(error){
+        return console.log("failed to connect data base")
     }
-    catch {
-       return []
-    }
-}
- 
-const savedata = (alldata)=>{ //obj to json
-    const savejsondata = JSON.stringify(alldata)
-    fs.writeFileSync("data.json" , savejsondata)
-}
-const deleteperson = (id) =>{
-  const alldata = loadData() ; 
-  const tracedata = alldata.filter((obj) =>{
-    return obj.id != 4 && obj.id !=6
-  })
-  console.log(tracedata)
-  savedata(tracedata)
-}
-const listperson =() =>{
-    const alldata = loadData() ; 
-    alldata.forEach((obj) =>{
-        console.log(obj.fname , obj.lname , obj.city)
-    });
-}
-const readData = (id) =>{
-    const alldata = loadData() ; 
-    const wanteddata = alldata.find((obj)=>{
-        return obj.id == 5 
+    console.log("success to connect")
+    const db = data.db(dbname)
+    ////////////////////////////////
+    db.collection("users").insertOne({
+        name : "nada" , 
+        age : 21
+    } , (error ,data) =>{
+        if(error){
+            console.log("failed inserted")
+        }
+        console.log(data.insertedId)
     })
-    console.log(wanteddata)
+    ////////////////////////////////////////////
+    db.collection("users").insertOne({
+        name : "mady ",
+        age :22
+    }  , (error , data) =>{
+        if(error){
+         console.log("failed to insert")
+        }
+        console.log(data.insertedId)
+    })
+
+    ///////////////////////////////////////////
+    db.collection("users").insertMany(
+        [
+            {
+                name : "islam " , 
+                age : 25
+            } , 
+            {
+                name :"wael" , 
+                age : 30
+            } , 
+            {
+                name : "rana" , 
+                age : 25
+            } , 
+            {
+                name : "tamer" , 
+                age : 25
+            } , 
+            {
+                name : "amr" , 
+                age : 25
+            } , 
+            {
+                name : "mahmoud" , 
+                age : 25
+            } , 
+            {
+                name : "rana" , 
+                age : 40
+            } , 
+            {
+                name : "hashem" , 
+                age : 50
+            } ,  {
+                name : "nader" , 
+                age : 33
+            } , 
+            {
+                name : "gazer" , 
+                age : 35
+            } , 
+        ] , (error , data)=>{
+            if(error){
+                console.log("failed to insert")
+            }
+            console.log(data.insertedIds)
+        }
+    )
+      ////////////////////////////////
+      /*db.collection("users").find({age:25}).toArray((errorr , data)=>{
+        if(errorr){
+         console.log("failed to find it")
+        }
+        console.log(data)
+
+      })
+      db.collection("users").find({age:25}).limit(3).toArray((errorr , data)=>{
+        if(errorr){
+         console.log("failed to find it")
+        }
+        console.log(data)
+
+      })
+      */
+      //////////////////////////////////////////////////////////
+      /*
+      db.collection("users").updateOne({_id:mongodb.ObjectId("64db2064825052e7fd40635c")}, {
+        $set : {name : "ramez"} , 
+        $inc : {age : 20 }
+      } , (error , data) =>{
+        if(error){
+            console.log("cant update")
+        }
+        console.log(data)
+        
+      })*/
+      ///////////////////////////////////////////////
+      /*
+      db.collection("users").updateMany({} , {
+        $inc : {age : 10}
+      }  ,(error , data) =>{
+        if(error){
+            console.log("cant update")
+        }
+        console.log(data.modifiedCount)
+    })
+    */
+    ///////////////////////////////////////////
+    db.collection("users").deleteMany({age:45} )
+    .then((data)=>{console.log(data.deletedCount)})
+    .catch((error) =>{console.log(error)})
 }
-module.exports ={
-    addperson , 
-    deleteperson , 
-    listperson , 
-    readData 
-}
+)
+
